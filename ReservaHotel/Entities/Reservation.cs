dddd974cd32d70aa10284a8c6c72026ac1f55f57
Kle3er Alves <klebergithub@gmail.com>
2022-01-10 16:07:50 -0300

    CODIGO RODANDO SEM O TRATAMENTO DE ERROS

---
+++ ReservaHotel/Entities/Reservation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReservaHotel.Entities
+{
+    class Reservation
+    {   //SUPERCLASSE
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        //CONSTRUTORES
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        //METODOS DURACAO
+        public int Duration()
+        {
+           TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        //METODO UPDATE -ATUALIZA O OBJETO
+        public void UpdateDates (DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        //METODO TO STRING PARA IMPRIMIR O OBJETO NA TELA
+        public override string ToString()
+        {
+            return "Room  "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights ";
+        }
+    }
+}
--- ReservaHotel/Program.cs
+++ ReservaHotel/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ReservaHotel.Entities;
+using System.Globalization;
 
 namespace ReservaHotel
 {
@@ -6,7 +8,37 @@ namespace ReservaHotel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Room number: ");
+            int room = int.Parse(Console.ReadLine());
+
+            Console.Write("Check in date (dd/MM/yyyy):");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check out date (dd/MM/yyyy):");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(room, checkIn, checkOut);//recebe os parametros digitados pelo usuario e enviar para a 
+            //variavel "reservation" onde os parametros serão alocados nos atributos e o metodo ToString é ativado
+            Console.WriteLine("Reservation: "+ reservation); //Imprime o objeto reservation na tela
+
+            //TRATAMENTOS DE ERROS if else
+
+            //Update 
+            Console.Write(" ");
+            Console.Write("Enter data to update the reservation: ");
+
+            Console.Write("Check in date (dd/MM/yyyy):");
+            checkIn = DateTime.Parse(Console.ReadLine()); //variavel com o tipo ja declarado, apenas utilizar
+            Console.Write("Check out date (dd/MM/yyyy):");
+            checkOut = DateTime.Parse(Console.ReadLine()); //variavel com o tipo ja declarado, apenas utilizar
+
+            //TRATAMENTOS DE ERROS if else
+
+
+            //ATUALIZANDO A RESERVA
+            reservation.UpdateDates(checkIn, checkOut);//chama o metodo UpdateDates
+            Console.WriteLine("Reservation:  "+ reservation);
+
+
         }
     }
 }
